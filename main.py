c14a657185524b8890bf944fd61e254f47331238
Don <don18donis@gmail.com>
2025-07-19 11:23:11 -0500

    Particle System without an Emitter, but with less messy code and with gravity applied.

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+import particle
+
+pygame.init()
+
+clock = pygame.time.Clock()
+FPS = 60
+
+DISPLAYWIDTH = 800
+DISPLAYHEIGHT = 800
+
+DISPLAY = pygame.display.set_mode((DISPLAYWIDTH, DISPLAYHEIGHT))
+
+# colors 
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+YELLOW = (128, 128, 0)
+
+running = True
+
+
+# particle values
+pos_x = 400
+pos_y = 400
+color = RED
+width = 30
+height = 30
+angle_x = 90
+angle_y = 90
+speed = 40
+gravity = 2
+life = 5
+
+obj = particle.RectangularParticle(pos_x, pos_y, color, width, height, angle_x, angle_y, speed, gravity, life)
+moving_group = pygame.sprite.Group()
+moving_group.add(obj)
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    DISPLAY.fill(BLACK)
+
+    moving_group.update()
+    moving_group.draw(DISPLAY)
+    clock.tick(FPS)
+    pygame.display.flip()
---
+++ particle.py
@@ -0,0 +1,70 @@
+import pygame
+import math
+
+
+class RectangularParticle(pygame.sprite.Sprite):
+
+    def applied_gravity(self):
+        if self.gravity != 0:
+            self.gravity_applied = True
+
+    def get_age_ratio(self):
+        self.age_ratio = self.life/self.original_life
+
+    def set_alpha(self):
+        self.alpha = self.age_ratio
+
+    def get_alpha(self):
+        # alpha in pygame ranges from 0-255
+        # we need to convert it to make it work
+        return self.alpha* 255
+    
+    def set_position(self):
+
+        if self.gravity_applied ==  True:
+            self.vel_y += self.gravity* self.time
+
+        self.pos_x += self.vel_x* self.time
+        self.pos_y += self.vel_y* self.time
+
+    def change_time(self, delta_time):
+        self.time += delta_time
+        self.life -= delta_time
+
+    def set_rect_center(self):
+        self.rect.center = (self.pos_x, self.pos_y)
+
+    def update(self):
+        self.set_rect_center()
+        self.change_time(1/60)
+        self.get_age_ratio()
+        self.set_alpha()
+        self.image.set_alpha(self.get_alpha())
+        self.set_position()
+
+
+    def __init__(self, pos_x, pos_y, color, width, height, angle_x, angle_y, speed, gravity, life):
+        super().__init__()
+        self.image  = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+        # might want to change angles in the future throughout program running
+        # math module originally sticks to radians, so you need to convert to degrees if you want degrees
+        self.vel_x = speed* math.cos(angle_x* math.pi/ 180)
+        self.vel_y = -1* speed* math.sin(angle_y* math.pi/180)
+
+        self.gravity = self.acceleration = gravity
+        self.gravity_applied = False
+        self.applied_gravity()
+
+        self.original_life = self.life = life
+        self.alpha = self.age_ratio = 1
+        self.time = 0
+
+        print(self.vel_x)
+        print(self.vel_y)
